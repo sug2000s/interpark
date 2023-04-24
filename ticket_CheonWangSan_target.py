21bdd5e28f2883f12d6ab9c9e1df4838f7fd8ca2
CNS\sug2000s <sug2000s@gmail.com>
2023-04-24 22:18:43 +0900

    천왕산으로 합치고 메모리 조정합

--- ticket_CheonWangSan_target.py
+++ ticket_CheonWangSan_target.py
@@ -50,7 +50,7 @@ def log_in():
         print("got exception(log_in)")
 
 
-def move_to_ticket_page():
+def move_to_ticket_Cheon_page():
     try:
         공연코드 = 21012652
         userSearch = f"https://tickets.interpark.com/goods/{공연코드}#"
@@ -99,8 +99,6 @@ def move_to_ticket_page():
                 day = driver.find_element(By.XPATH, i).text
                 asyncio.run(sendTelegram('5월' + day))  # 봇 실행하는 코드
 
-        move_to_ticket_page()
-
 
     except Exception as e:
         print(e)
@@ -108,5 +106,94 @@ def move_to_ticket_page():
         asyncio.run(sendTelegram('오류가 발생 하였습니다.'))  # 봇 실행하는 코드
 
 
+def move_to_ticket_No_page():
+    try:
+        공연코드 = 22011899
+        userSearch = f"https://tickets.interpark.com/goods/{공연코드}#"
+        driver.get(userSearch)
+        time.sleep(0.7)
+        driver.find_element(By.XPATH, '//*[@id="popup-prdGuide"]/div/div[3]/button').click()
+
+        labels = ['//*[@id="productSide"]/div/div[1]/div[1]/div[2]/div/div/div/div/ul[3]/li[35]'
+            , '//*[@id="productSide"]/div/div[1]/div[1]/div[2]/div/div/div/div/ul[3]/li[36]'
+                  ]
+        for i in labels:
+            driver.find_element(By.XPATH, i).click()
+            time.sleep(1)
+            driver.find_element(By.XPATH, '//*[@id="productSide"]/div/div[1]/div[2]/div[2]/div[1]/ul/li[1]').click()
+
+            result = driver.find_element(By.XPATH,
+                                         '//*[@id="productSide"]/div/div[1]/div[2]/div[2]/div[2]/ul/li[1]/span').text
+
+            result2 = driver.find_element(By.XPATH,
+                                          '//*[@id="productSide"]/div/div[1]/div[2]/div[2]/div[2]/ul/li[2]/span').text
+            if result == '매진' and result2 == '매진':
+                time.sleep(2)
+            else:
+                day = driver.find_element(By.XPATH, i).text
+                asyncio.run(sendTelegram('4월' + day))  # 봇 실행하는 코드
+
+        ##5월 클릭
+        driver.find_element(By.XPATH,
+                            '//*[@id="productSide"]/div/div[1]/div[1]/div[2]/div/div/div/div/ul[1]/li[3]').click()
+        labels2 = ['//*[@id="productSide"]/div/div[1]/div[1]/div[2]/div/div/div/div/ul[3]/li[6]'
+            , '//*[@id="productSide"]/div/div[1]/div[1]/div[2]/div/div/div/div/ul[3]/li[7]'
+            , '//*[@id="productSide"]/div/div[1]/div[1]/div[2]/div/div/div/div/ul[3]/li[14]'
+            , '//*[@id="productSide"]/div/div[1]/div[1]/div[2]/div/div/div/div/ul[3]/li[21]'
+            , '//*[@id="productSide"]/div/div[1]/div[1]/div[2]/div/div/div/div/ul[3]/li[28]'
+                   ]
+
+        for i in labels2:
+            driver.find_element(By.XPATH, i).click()
+            time.sleep(1)
+            driver.find_element(By.XPATH, '//*[@id="productSide"]/div/div[1]/div[2]/div[2]/div[1]/ul/li[1]').click()
+
+            result = driver.find_element(By.XPATH,
+                                         '//*[@id="productSide"]/div/div[1]/div[2]/div[2]/div[2]/ul/li[1]/span').text
+
+            result2 = driver.find_element(By.XPATH,
+                                          '//*[@id="productSide"]/div/div[1]/div[2]/div[2]/div[2]/ul/li[2]/span').text
+            if result == '매진' and result2 == '매진':
+                time.sleep(2)
+            else:
+                day = driver.find_element(By.XPATH, i).text
+                asyncio.run(sendTelegram('5월' + day))  # 봇 실행하는 코드
+
+    except Exception as e:
+        print(e)
+        print("got exception(move_to_ticket_page)")
+        asyncio.run(sendTelegram('오류가 발생 하였습니다.'))  # 봇 실행하는 코드
+
+
+def move_to_ticket_Do_page():
+    try:
+
+        userSearch = f"https://reserve.gmuc.co.kr/user/camp/campReservation.do?menu=d&menuFlag=C"
+        driver.get(userSearch)
+        time.sleep(10)
+        labels = ['//*[@id="sub_wrap_full"]/div/table/tbody/tr[6]/td[7]/div[2]/a'
+             , '//*[@id="sub_wrap_full"]/div/table/tbody/tr[6]/td[7]/div[3]/a'
+             , '//*[@id="sub_wrap_full"]/div/table/tbody/tr[7]/td[1]/div[2]/a'
+             , '//*[@id="sub_wrap_full"]/div/table/tbody/tr[7]/td[1]/div[3]/a'
+                  ]
+        for i in range(0,len(labels)):
+            result = driver.find_element(By.XPATH, labels[i]).text
+
+            if i%2 == 0 and result == 'A구역 잔여 데크: 0':
+                time.sleep(2)
+            elif i%2 == 1 and result == 'B구역 잔여 데크: 0':
+                time.sleep(2)
+            else:
+                asyncio.run(sendTelegram('4월' ))  # 봇 실행하는 코드
+    except Exception as e:
+        print(e)
+        print("got exception(move_to_ticket_page)")
+        asyncio.run(sendTelegram('오류가 발생 하였습니다.'))  # 봇 실행하는 코드
+
+
 log_in()
-move_to_ticket_page()
+
+while (True):
+    move_to_ticket_Cheon_page()
+    move_to_ticket_No_page()
+    move_to_ticket_Do_page()
